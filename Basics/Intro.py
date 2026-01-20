207cc00dd93ae40e1b6fa4e04d5eb331535c2ec2
santoshsatkartech <santoshsatkartech2026@gmail.com>
2026-01-20 09:46:03 +0530

    Complete Basics

--- Basics/Intro.py
+++ Basics/Intro.py
@@ -1,10 +1,63 @@
 print ("Hello, nyn!")
 
+# Variables and Datatypes
 name = "Nayan"
 age = 23
 is_coding = True
-
 print(f"my name is { name } and my age is      {age}" )
-
 print("nayan")
 print("is coding")
+
+# Input and Outputs
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+#Operators in python
+a = 10
+b = 5
+
+# Arithmetic Operators
+print("Addition:", a + b)       #15
+print("Subtraction:", a - b)    #5
+print("Multiplication:", a * b) #50
+print("Division:", a / b)       #2.0
+
+# Comparison Operators
+print("Equal:", a == b)         #False
+print("Not Equal:", a != b)     #True
+print("Greater than:", a > b)   #True
+print("Less than:", a < b)      #False
+print("Greater than or equal to:", a >= b)    #True
+print("Less than or equal to:", a <= b)   #False
+
+# Logical Operators
+print("AND:", a > 5 and b < 10) #True 
+print("OR:", a > 15 or b < 10)  #True
+print("NOT:", not(a > 5))       #False
+
+#assignment Operators
+c = 10
+c += 5
+print("c after += 5:", c)     #15
+c -= 3
+print("c after -= 3:", c)     #12
+c *= 2
+print("c after *= 2:", c)     #24
+c /= 4
+print("c after /= 4:", c)     #6.0
+c %= 3
+print("c after %= 3:", c)     #0.0
+c **= 2
+print("c after **= 2:", c)    #0.0
+c //= 2
+print("c after //= 2:", c)    #0.0
+
+#bitwise Operators
+x = 6  # In binary: 110
+y = 3  # In binary: 011
+print("Bitwise AND:", x & y)    #2
+print("Bitwise OR:", x | y)     #7
+print("Bitwise XOR:", x ^ y)    #5
+print("Bitwise NOT:", ~x)       #-7
+print("Left Shift:", x << 1)    #12
+print("Right Shift:", x >> 1)   #3
